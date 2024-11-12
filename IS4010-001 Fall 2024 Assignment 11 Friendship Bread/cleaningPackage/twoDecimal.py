027f581b41219f0ee1d8fa6c5829540c796b0fbd
Alex Patton <pattona6@mail.uc.edu>
2024-11-12 09:19:01 -0500

    begun the process of formatting the fuel quantity column to 2 decomal places

--- IS4010-001 Fall 2024 Assignment 11 Friendship Bread/cleaningPackage/twoDecimal.py
+++ IS4010-001 Fall 2024 Assignment 11 Friendship Bread/cleaningPackage/twoDecimal.py
@@ -1,1 +1,24 @@
+# twoDecimal.py
 
+import csv
+
+# open and read csv data
+with open('fuelPurchaseData.csv', mode = 'r', newline = '') as infile:
+
+    reader = csv.reader(infile)
+    header = next(reader)  # read the header row
+    rows = list(reader) # real all rows
+
+    # convert the column value to a float and format it to 2 decimal places
+    columnName = 'Fuel Quantity'
+    for row in rows:
+        try:
+            row[columnName] = "{:.2f}".format(float(row[columnName]))
+        except ValueError:
+            pass
+
+    with open('cleanedData.csv', mode='w', newline='') as outfile:
+        fieldnames = reader.fieldnames  # Get the fieldnames (headers) from the reader
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()  # Write the header
+        writer.writerows(rows)  # Write the modified rows
